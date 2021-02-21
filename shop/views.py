9dc1a1b07a79ccdce4ae1aab045bfbffb3ddee89
friendtu <friendtu@gmail.com>
2021-02-21 22:08:41 +0800

    add view of list

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Product,Category
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+def product_list(request,category_slug=None):
+    categories=Category.objects.all()
+    products=Product.objects.all(available=True)
+    if category_slug:
+        category=get_object_or_404(Category,slug=category_slug)
+        products=products.filter(category=category)
+    return render(request,'shop/product/list.html',{
+                        "categories":categories,
+                        "category":category,
+                        "products":products})
+
+def product_detail(request,id,slug):
+    pass
