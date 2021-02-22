f66c26a66cfdc6770dd90f07c2842d19e232c560
friendtu <friendtu@gmail.com>
2021-02-22 14:58:02 +0800

    add url define

--- shop/views.py
+++ shop/views.py
@@ -5,13 +5,17 @@ from django.shortcuts import get_object_or_404
 # Create your views here.
 def product_list(request,category_slug=None):
     categories=Category.objects.all()
-    products=Product.objects.all(available=True)
+    products=Product.objects.filter(available=True)
     if category_slug:
         category=get_object_or_404(Category,slug=category_slug)
         products=products.filter(category=category)
-    return render(request,'shop/product/list.html',{
+        return render(request,'shop/product/list.html',{
                         "categories":categories,
                         "category":category,
+                        "products":products})
+    else:
+        return render(request,'shop/product/list.html',{
+                        "categories":categories,
                         "products":products})
 
 def product_detail(request,id,slug):
