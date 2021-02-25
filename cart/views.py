773fa484c576941d92cafc5a2ed3320192f2d77e
friendtu <friendtu@gmail.com>
2021-02-25 16:19:09 +0800

    add view for cart -1

--- cart/views.py
+++ cart/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_POST
+from .cart import Cart
 
 # Create your views here.
+
+def cart_detail(request):
+    cart=Cart(request)
+    return render(request,'cart/detail.html',{
+        'cart':cart,
+    })
+
+@require_POST
+def cart_add(request, product_id):
+    pass
+
+def cart_remove(request,product_id):
+    pass
