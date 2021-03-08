66fb48e1157577faec5a90f376782221c0687d0a
friendtu <friendtu@gmail.com>
2021-03-08 19:21:44 +0800

    update js of process.html

--- payment/views.py
+++ payment/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,get_object_or_404
 import braintree
 from orders.models import Order
+
 # Create your views here.
 
 
@@ -13,8 +14,21 @@ def payment_process(request):
         pass
     else:
         #client_token=braintree.ClientToken.generate()
+        #client_token=braintree.
+
+        gateway = braintree.BraintreeGateway(
+            braintree.Configuration(
+                braintree.Environment.Sandbox,
+                merchant_id="c8ysmy3956rnqhms",
+                public_key="b74vs7gkt33gsxyf",
+                private_key="3d6098d7bc6611fcfea8303799338716s"
+            )
+        )
+        client_token = gateway.client_token.generate()
+
         return render(request,'payment/process.html',{
                             'order':order,
+                            'client_token':client_token
                     })
 
 def payment_done(request):
